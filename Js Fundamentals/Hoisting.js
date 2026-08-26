3fdfee6939c65d11fbcf260ced31843080fa4b13
David Phillipus <david.n.phillipus@gmail.com>
2026-08-27 01:33:07 +0200

    Add Js Fundamentals\Hoisting.js

---
+++ Js Fundamentals/Hoisting.js
@@ -0,0 +1,12 @@
+let a = 1;
+console.log(  sum(a , 2))
+
+
+function sum(a , b){
+    return a + b
+}
+
+//Hoisting takes all the functions and take it to the top so even if you call a function before its defined the code would still work
+// This is however different for arow functions because arrow functions are mostly difined as variables and variables are never hoisted
+
+
