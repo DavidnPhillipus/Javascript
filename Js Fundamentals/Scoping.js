76ba68a67e9949cdf5253bef19d3bca4f39698bf
David Phillipus <david.n.phillipus@gmail.com>
2026-08-27 01:33:08 +0200

    Add Js Fundamentals\Scoping.js

---
+++ Js Fundamentals/Scoping.js
@@ -0,0 +1,11 @@
+function sayHi(name){
+    let result = "Hi " + name
+}
+
+
+let result = "David"
+
+sayHi(result);
+
+//notice thay we have 2 results one is in the global scope and another is in the local scope
+//
