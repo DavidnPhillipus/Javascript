2b65ccd22dec85191b2c3f1d9c825c24bbbfdd4d
David Phillipus <david.n.phillipus@gmail.com>
2026-08-28 11:24:04 +0200

    Add Introduction to JavaScript/comments.js

---
+++ Introduction to JavaScript/comments.js
@@ -0,0 +1,26 @@
+// This is a single-line comment
+
+/*
+    This is a multi-line comment.
+    You can use it to comment out multiple lines of code.
+*/
+
+// Single-line comments are useful for brief explanations or notes
+
+let x = 5; // This is an inline comment
+
+/*
+    Multi-line comments are useful for more detailed explanations
+    or for temporarily disabling blocks of code.
+*/
+
+function add(a, b) {
+    return a + b; // This function adds two numbers
+}
+
+/*
+    You can also use comments to explain complex logic
+    or to leave reminders for yourself or other developers.
+*/
+
+console.log(add(2, 3)); // Output: 5
