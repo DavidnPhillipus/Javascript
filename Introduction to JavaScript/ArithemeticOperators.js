5c5d108558f88f71a0007816525129fe6ba88704
David Phillipus <david.n.phillipus@gmail.com>
2026-08-28 11:24:02 +0200

    Add Introduction to JavaScript/ArithemeticOperators.js

---
+++ Introduction to JavaScript/ArithemeticOperators.js
@@ -0,0 +1,32 @@
+// Arithmetic Operators in JavaScript
+
+// Addition (+)
+let sum = 10 + 5; // 15
+
+// Subtraction (-)
+let difference = 10 - 5; // 5
+
+// Multiplication (*)
+let product = 10 * 5; // 50
+
+// Division (/)
+let quotient = 10 / 5; // 2
+
+// Modulus (%)
+let remainder = 10 % 3; // 1
+
+// Exponentiation (**)
+let power = 2 ** 3; // 8
+
+// Increment (++)
+let increment = 5;
+increment++; // 6
+
+// Decrement (--)
+let decrement = 5;
+decrement--; // 4
+
+// Notes:
+// 1. Arithmetic operators are used to perform arithmetic operations on numbers.
+// 2. The addition operator (+) can also be used to concatenate strings.
+let concatenatedString = "Hello" + " " + "World"; // "Hello World"
