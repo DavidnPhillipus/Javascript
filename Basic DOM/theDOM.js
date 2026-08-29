ba4aa17da1e1fabea9e50f50d6e8062dac081cf3
David Phillipus <david.n.phillipus@gmail.com>
2026-08-30 00:17:18 +0200

    Add Basic DOM\theDOM.js

---
+++ Basic DOM/theDOM.js
@@ -0,0 +1,4 @@
+console.log(document)//It log out all of the html of our page
+const element = document.createElement("span")
+element.innerText = "I am inevitable"
+document.body.appendChild(element)
